e4de57f6694fabd6674dd24134623e14e8b16669
NHarika41 <98961687+Nharika41@users.noreply.github.com>
2022-09-29 20:27:40 +0530

    Update WaterJug.py

--- WaterJug.py
+++ WaterJug.py
@@ -1,11 +1,4 @@
 '''A Water Jug Problem: You are given two jugs, a 4-litre one and a 3-litre one, a pump which has unlimited water which you can use to fill the jug, and the ground on which water may be poured. Neither jug has any measuring markings on it. How can you get exactly 2 litre of water in the 4-gallon jug? 
-Let X represent the content of the water in 4- g litre jug. 
-Let Y represent the content of the water in 3-g litre jug.
-Start State : (0,0)
-Goal State : (2,n) for any value of n
-So, to solve this problem, following set of rules were proposed:
-Production rules for solving the water jug problem
-Here, let x denote the 4-gallon jug and y denote the 3-gallon jug.
 '''
 
 print("A is a 4 Gallons jug")
@@ -57,6 +50,75 @@ for i in range(9):
         B.pop(0)#[2][0]
         print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
         print("\n")
+        
+    '''A Water Jug Problem: You are given two jugs, a 8-litre one and a 6-litre one, a pump which has unlimited water which you can use to fill the jug, and the ground on which water may be poured. Neither jug has any measuring markings on it. How can you get exactly 4 litre of water in the 8-gallon jug? 
+ '''  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        print("A is a 8 Gallons jug")
+print("B is a 6 Gallons jug")
+A=[]
+B=[]
+print("\n")
+print("CONDITIONS:")
+print("There are no markings on the jug A and jug B")
+print("Atlast we need to have 4 Gallons of water in jug A")
+print("\n")
+for i in range(9):
+    a=int(input("Amount of Water you want to remove from A:"))
+    a0=int(input("Amount of water you want to add to A:"))
+    b=int(input("Amount of water you want to remove to B:"))
+    b0=int(input("Amount of water you want to add to B:"))
+    if(i==1):
+        A.append(a0)
+        print("\n")
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==2):
+        B.append(A[0]-a)
+        A.pop(0)
+        A.append(a)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==3):
+        B.pop(0)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==4):
+        B.append(A[0])
+        A.pop(0)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==5):
+        A.append(a0)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==6):
+        B.append(B[0]+A[0]-a)
+        A.append(A[0]-a)
+        B.pop(0)
+        A.pop(0)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+    if(i==7):
+        B.pop(0)
+        print("In jug-A we are having",A,"gallons of water","\n","In jug-B we are having",B,"gallons of water")
+        print("\n")
+
+        
+        
+        
+    
+
 
         
         
